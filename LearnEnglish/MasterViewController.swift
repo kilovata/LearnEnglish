4d854aae4162fb791da8e76d5525b8eea1621c87
Sveta Polishchuk <s.polishchuk@infotech.team>
2017-05-12 11:04:08 +0300

    added all files

--- LearnEnglish/MasterViewController.swift
+++ LearnEnglish/MasterViewController.swift
@@ -7,188 +7,146 @@
 //
 
 import UIKit
-import CoreData
 
-class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+enum DictionaryType: String
+{
+    case words
+    case verbs
+    case idioms
+    case phrases
+}
 
-    var detailViewController: DetailViewController? = nil
-    var managedObjectContext: NSManagedObjectContext? = nil
+enum Language: String
+{
+    case eng
+    case rus
+}
 
+class MasterViewController: UITableViewController
+{
+    var currentWords = [[String : String]]()
+    var currentType = DictionaryType.words
+    var currentLanguge = Language.eng
+    var showTranslation = true
+    
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
+        
+        title = "Eng -> Rus"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(changeWordTypes))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .rewind, target: self, action: #selector(changeMainLanguage))
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.tableFooterView = UIView()
+        
+        setupDictionary()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
-        super.viewWillAppear(animated)
-    }
-
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    func insertNewObject(_ sender: Any) {
-        let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
-             
-        // If appropriate, configure the new managed object.
-        newEvent.timestamp = NSDate()
-
-        // Save the context.
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    
+    func setupDictionary()
+    {
+        if let path = Bundle.main.path(forResource: currentType.rawValue, ofType: "plist") {
+            currentWords = NSArray(contentsOfFile: path) as! [[String : String]]
         }
     }
-
-    // MARK: - Segues
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = self.fetchedResultsController.object(at: indexPath)
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
+    
+    func changeWordTypes()
+    {
+        let alert = UIAlertController(title: "Choose type", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: DictionaryType.words.rawValue, style: UIAlertActionStyle.default, handler: { (action) in
+            self.currentType = .words
+            self.changeDictionary()
+        }))
+        
+        alert.addAction(UIAlertAction(title: DictionaryType.verbs.rawValue, style: UIAlertActionStyle.default, handler: { (action) in
+            self.currentType = .verbs
+            self.changeDictionary()
+        }))
+        
+        alert.addAction(UIAlertAction(title: DictionaryType.idioms.rawValue, style: UIAlertActionStyle.default, handler: { (action) in
+            self.currentType = .idioms
+            self.changeDictionary()
+        }))
+        
+        alert.addAction(UIAlertAction(title: DictionaryType.phrases.rawValue, style: UIAlertActionStyle.default, handler: { (action) in
+            self.currentType = .phrases
+            self.changeDictionary()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: { (action) in
+            self.dismiss(animated: true, completion: {
+                
+            })
+        }))
+            
+        present(alert, animated: true) { 
+            
         }
     }
+    
+    func changeMainLanguage()
+    {
+        switch currentLanguge
+        {
+            case .eng:
+            currentLanguge = .rus
+            title = "Rus -> Eng"
+            break
+            
+            case .rus:
+            currentLanguge = .eng
+            title = "Eng -> Rus"
+            break
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func changeDictionary()
+    {
+        setupDictionary()
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableViewDataSource, UITableViewDelegate
 
-    // MARK: - Table View
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return self.fetchedResultsController.sections?.count ?? 0
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionInfo = self.fetchedResultsController.sections![section]
-        return sectionInfo.numberOfObjects
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return currentWords.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let event = self.fetchedResultsController.object(at: indexPath)
-        self.configureCell(cell, withEvent: event)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SubtitleCell", for: indexPath) as! SubtitleCell
+
+        let item = currentWords[indexPath.row]
+        cell.labelTitle.text = currentLanguge == .eng ? item["eng"] : item["rus"]
+        
+        if showTranslation {
+            cell.labelSubTitle.text = currentLanguge == .eng ? item["rus"] : item["eng"]
+        } else {
+            cell.labelSubTitle.text = nil
+        }
+        
         return cell
     }
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        showTranslation = !showTranslation
+        tableView.reloadData()
     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let context = self.fetchedResultsController.managedObjectContext
-            context.delete(self.fetchedResultsController.object(at: indexPath))
-                
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
-    func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
-    }
-
-    // MARK: - Fetched results controller
-
-    var fetchedResultsController: NSFetchedResultsController<Event> {
-        if _fetchedResultsController != nil {
-            return _fetchedResultsController!
-        }
-        
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
-        
-        // Set the batch size to a suitable number.
-        fetchRequest.fetchBatchSize = 20
-        
-        // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
-        
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        // Edit the section name key path and cache name if appropriate.
-        // nil for section name key path means "no sections".
-        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext!, sectionNameKeyPath: nil, cacheName: "Master")
-        aFetchedResultsController.delegate = self
-        _fetchedResultsController = aFetchedResultsController
-        
-        do {
-            try _fetchedResultsController!.performFetch()
-        } catch {
-             // Replace this implementation with code to handle the error appropriately.
-             // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-             let nserror = error as NSError
-             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-        
-        return _fetchedResultsController!
-    }    
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
-
-    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        self.tableView.beginUpdates()
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-        switch type {
-            case .insert:
-                self.tableView.insertSections(IndexSet(integer: sectionIndex), with: .fade)
-            case .delete:
-                self.tableView.deleteSections(IndexSet(integer: sectionIndex), with: .fade)
-            default:
-                return
-        }
-    }
-
-    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-        switch type {
-            case .insert:
-                tableView.insertRows(at: [newIndexPath!], with: .fade)
-            case .delete:
-                tableView.deleteRows(at: [indexPath!], with: .fade)
-            case .update:
-                self.configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
-            case .move:
-                tableView.moveRow(at: indexPath!, to: newIndexPath!)
-        }
-    }
-
-    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
-        self.tableView.endUpdates()
-    }
-
-    /*
-     // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
-     
-     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-         // In the simplest, most efficient, case, reload the table view.
-         self.tableView.reloadData()
-     }
-     */
-
 }
 
---
+++ LearnEnglish/SubtitleCell.swift
@@ -0,0 +1,16 @@
+//
+//  SubtitleCell.swift
+//  LearnEnglish
+//
+//  Created by Vladimir Pronin on 5/12/17.
+//  Copyright © 2017 Kilovata. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SubtitleCell: UITableViewCell
+{
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelSubTitle: UILabel!
+}
